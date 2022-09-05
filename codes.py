cc1ae81352a39f6078883e33083f6871709cf1db
Ezenne <ezennewhytte@gmail.com>
2022-09-05 19:01:29 +0000

    first lines of cmd

---
+++ codes.py
@@ -0,0 +1,4 @@
+print("play me sound")
+print("balanced sound")
+print("3D surround system")
+print("noise cancellation efx")
---
+++ contributors.sh
@@ -0,0 +1,12 @@
+favor
+faith
+dominion
+simon
+sean
+seun
+ola
+ikenna
+whytte
+joel
+coleman
+
